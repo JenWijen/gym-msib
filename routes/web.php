007992fab4ef53b40bfb2abbd6d204751a9e016e
dbllyy <debyli271@gmail.com>
2024-05-24 09:46:37 +0700

    landing page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingpage.master');
 });
 
 Route::get('/dashboard', function () {
