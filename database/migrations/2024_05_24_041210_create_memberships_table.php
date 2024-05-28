84df7b8804ce260665479b34f5b6c49b4ad06883
JenWijen <dutawijdan2@gmail.com>
2024-05-28 21:28:49 +0700

    ulang

--- database/migrations/2024_05_24_041210_create_memberships_table.php
+++ database/migrations/2024_05_24_041210_create_memberships_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('memberships');
+        Schema::dropIfExists('membershipasd');
     }
 };
