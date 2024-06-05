46234edc72856ce0af591a58f8637f8216b0b6d8
Farid4l <4l4554d.03@gmail.com>
2024-06-05 11:52:53 +0700

    edit migration

---
+++ database/migrations/2024_06_05_031606_remove_packet_name_and_price_from_memberships_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('memberships', function (Blueprint $table) {
+            $table->dropColumn(['packet_name', 'price']);
+            $table->foreignId('member_id')->after('id')
+            ->constrained('members')
+            ->onDelete('restrict')
+            ->onUpdate('cascade');
+            $table->foreignId('package_id')->after('member_id')
+            ->constrained('member_packages')
+            ->onDelete('restrict')
+            ->onUpdate('cascade');
+            $table->date('startdate')->after('member_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('memberships', function (Blueprint $table) {
+            $table->string('packet_name');
+            $table->integer('price');
+            $table->dropColumn(['membership_id', 'startdate']);
+        });
+    }
+};
