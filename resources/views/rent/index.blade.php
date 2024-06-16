04f51bb8e3ed00b67911aa8a548de979d74df2ee
JenWijen <dutawijdan2@gmail.com>
2024-06-16 13:55:45 +0700

    rent admin aman jaya

--- resources/views/rent/index.blade.php
+++ resources/views/rent/index.blade.php
@@ -18,7 +18,7 @@
                         <table id="example" class="display expandable-table" style="width:100%">
                             <thead>
                                 <tr>
-                                    <th>ID</th>
+                                    <th>No</th>
                                     <th>Name</th>
                                     <th>Field Name</th>
                                     <th>Rental Hours</th>
@@ -27,24 +27,30 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                @php $i = 0; @endphp
                                 @foreach ($rental as $rent)
-                                    <tr>
-                                        <td>{{ $rent->id }}</td>
-                                        <td>{{ $rent->user->name }}</td>
-                                        <td>{{ $rent->rpackage->field_name }}</td>
-                                        <td>{{ $rent->rent_hours }} Hours</td>
-                                        <td>{{ \Carbon\Carbon::parse($rent->startdate)->locale('id')->isoFormat('D MMMM YYYY') }}</td>
-
-                                        <td>
-                                            <form action="{{ route('rent_book.destroy', $rent->id) }}" method="POST">
-                                                <a class="btn btn-info" href="{{ route('rent_book.show', $rent->id) }}">Show</a>
-                                                <a class="btn btn-primary" href="{{ route('rent_book.edit', $rent->id) }}">Edit</a>
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
+                                    @if ($rent->user && $rent->rpackage)
+                                        <tr>
+                                            <td>{{ ++$i }}</td>
+                                            <td>{{ $rent->user->name }}</td>
+                                            <td>{{ $rent->rpackage->field_name }}</td>
+                                            <td>{{ $rent->rent_hours }} Hours</td>
+                                            <td>{{ \Carbon\Carbon::parse($rent->startdate)->locale('id')->isoFormat('D MMMM YYYY') }}</td>
+                                            <td>
+                                                <form action="{{ route('rent_book.destroy', $rent->id) }}" method="POST">
+                                                    <a class="btn btn-info" href="{{ route('rent_book.show', $rent->id) }}">Show</a>
+                                                    <a class="btn btn-primary" href="{{ route('rent_book.edit', $rent->id) }}">Edit</a>
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @else
+                                        <tr>
+                                            <td colspan="5">Incomplete rental information for rental ID: {{ $rent->id }}</td>
+                                        </tr>
+                                    @endif
                                 @endforeach
                             </tbody>  
                         </table>
