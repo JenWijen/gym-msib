dfba16ea9d73947e306b038967af5c2f29848247
JenWijen <dutawijdan2@gmail.com>
2024-06-18 21:13:48 +0700

    last pembayarannn

--- resources/views/user/footer.blade.php
+++ resources/views/user/footer.blade.php
@@ -34,11 +34,7 @@
     <div class="container copyright text-center mt-4">
       <p>© <span>Copyright</span> <strong class="sitename">GymFit</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="{{asset('user')}}/https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by Group 5 Capstone</a>
       </div>
     </div>
 
